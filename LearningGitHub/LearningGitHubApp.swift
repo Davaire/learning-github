3cbf2e0d76c395cb4b8c37fae000a51253869707
Davaire <davairebourgeois>
2021-11-04 16:55:00 -0500

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Davaire Bourgeois on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
